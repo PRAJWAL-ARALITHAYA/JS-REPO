b1f761d1d60b8348348d61f822a071179518f2cf
Prajwal Krishna A <prajwalaralithaya@gmail.com>
2025-08-05 15:26:31 +0000

    learned string methods and interpolation way of concatinating strings

---
+++ basics/string.js
@@ -0,0 +1,11 @@
+let name= new String("       prajwal krishna aralithaya      ")
+console.log(name.trim().split(' '))
+// like python trim and split is used in js
+const age=19
+const college=new String("SIT")
+let sentence=`my name is ${name.trim()} and i am ${age} year old currently studying in ${college}`;
+const new_name=name.replaceAll('a','u');
+console.log(new_name);
+// replace() just replace one instance at the same time replaceAll() replace all instances!!
+let word=new String("harry")
+console.log(`the word "${word}" ${sentence.includes(word)?"is":"is not"} in the sentence`)
