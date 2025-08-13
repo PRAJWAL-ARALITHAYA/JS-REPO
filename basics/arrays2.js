b67fb8bbd95436f93e850df0c5299a47d4d212ab
Prajwal Krishna A <prajwalaralithaya@gmail.com>
2025-08-13 15:40:19 +0000

    arrays_part_two

---
+++ basics/arrays2.js
@@ -0,0 +1,11 @@
+// let names=["prajwal","krishna","aralithaya"];
+// let other_names=["abhiram","vaishak","me"];
+// let concatinated_name=names.concat(other_names);
+// console.log(names.push(other_names));
+// console.log(names);
+// console.log(...names,...other_names)
+// let array=["prajwal","me",1,2,["i","o","u"],["abhi","hai","bye"]];
+// console.log(array.flat());
+// console.log(Array.isArray(1));
+let score=Array.of([1,2,3,4],(x)=>Math.pow(x,x));
+console.log(score);;
