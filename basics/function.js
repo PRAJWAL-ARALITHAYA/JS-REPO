739a65e9199d364d012dbf113f60a143592838a8
Prajwal Krishna A <prajwalaralithaya@gmail.com>
2025-09-05 06:31:46 +0000

    different methods of writing the function declaration

--- basics/function.js
+++ basics/function.js
@@ -1,5 +1,15 @@
 function add_numbers(num1, num2) {
   return num1 + num2;
+  // Normal function definition with function name;
 }
 let result = add_numbers(3, 4);
 console.log(result);
+let addTwo = function(num){
+  return num +2
+  // Anonymous function without name the value is stored in a varibale;
+}
+console.log(addTwo(5))
+
+let addThree = (num) => num +3;
+    // Arrow function can be used with anonymous function with a single paramter . return is optional
+console.log(addThree(5)); 
