d84e7a52f32609867de44e627699d43ef7430041
Prajwal Krishna A <prajwalaralithaya@gmail.com>
2025-09-06 13:00:49 +0000

    IIFE

---
+++ basics/iife.js
@@ -0,0 +1,12 @@
+//named iife
+(function hello(){
+    console.log("Hello World");
+})()
+;
+// unnamed IIFE
+(() => {
+    console.log("Hai guysss");
+})();
+// IIFE are immediately invoked function expression they are called instantly after definition 
+// ; is must if we write two IIFE continously
+// there are two types of IIFE named and unnamed 
