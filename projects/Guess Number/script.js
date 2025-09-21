ba0761629f40a97a702d52f9e2914e7a0666a637
Prajwal Krishna A <prajwalaralithaya@gmail.com>
2025-09-21 13:25:12 +0000

    updation

---
+++ projects/Guess Number/script.js
@@ -0,0 +1,7 @@
+document.body.style.backgroundColor = "#212121";
+document.body.style.color = "white";
+const container = document.querySelector(".container");
+container.style.border = "2px solid white";
+container.style.height = "500px";
+container.style.width = "500px";
+container.style.
